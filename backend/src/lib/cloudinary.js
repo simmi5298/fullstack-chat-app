9583ed42d74517629f6db56177fa3ea14302d5f4
simmi5298 <104838695+simmi5298@users.noreply.github.com>
2025-02-06 20:37:31 +0530

    changes in backend

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -5,7 +5,7 @@ config()
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARy_API_SECRET,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
 
 });
 
