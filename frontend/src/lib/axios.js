30055b6372e29c72e65e0b3f9a50fefe790cf450
simmi5298 <104838695+simmi5298@users.noreply.github.com>
2025-02-06 15:04:04 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 //we will use axios to create instance that we can use throughout our application
 import axios from "axios";
 export const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="devlopment" ? "http://localhost:5001/api":"/api",
+    baseURL:import.meta.env.MODE==="development" ? "http://localhost:5001/api":"/api",
     withCredentials:true,
 })
